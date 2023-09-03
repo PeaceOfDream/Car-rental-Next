de8710285a5fbaa3e10b0b6becf8d82d745f2ae6
Dima Bart <3088742@mail.ru>
2023-09-03 21:58:40 +0300

    add custom filters

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
-import { CustomFilter, Hero, SearchBar } from '@/components';
+import { CustomFilter, Hero, SearchBar, ShowMore } from '@/components';
 import {CarCard} from '@/components';
+import { fuels, yearsOfProduction } from '@/constants';
 import { fetchCars } from '@/utils';
 import Image from 'next/image';
 
-export default async function Home() {
-	const allCars = await fetchCars()
+export default async function Home({searchParams}) {
+	const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || '',
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || '',
+    limit: searchParams.limit || 10,
+    model: searchParams.model || '',
+  });
 
 	const isDataEmpty = !Array.isArray(allCars) || allCars.length <1 || !allCars
 
@@ -24,8 +31,8 @@ export default async function Home() {
 					<SearchBar />
 					<div className='home__filter-container'>
 
-						<CustomFilter title='fuel' />
-						<CustomFilter title='year' />
+						<CustomFilter title='fuel' options={fuels} />
+						<CustomFilter title='year' options={yearsOfProduction} />
 					</div>
 				</div>
 
@@ -39,6 +46,10 @@ export default async function Home() {
 						))}
 
 						</div>
+						<ShowMore 
+							pageNumber={(searchParams.limit || 10) / 10}
+							isNext={(searchParams.limit || 10) > allCars.length}
+						/>
 					</section>
 				) :
 				(
@@ -49,7 +60,7 @@ export default async function Home() {
 				)
 			}
 
-
+			
 			</div>
 		</main>
   );
--- utils/index.ts
+++ utils/index.ts
@@ -1,13 +1,15 @@
-import { CarProps } from '@/types';
+import { CarProps, FIltersProps } from '@/types';
 
-export async function fetchCars() {
+export async function fetchCars(filters: FIltersProps) {
+  const { manufacturer, year, limit, fuel, model } = filters;
+
   const headers = {
     'X-RapidAPI-Key':
       '714763140amsh88e1cad6961e8e9p14ca0ajsnebadbdaf66e8',
     'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
   };
   const response = await fetch(
-    'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=a4',
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
     {
       headers: headers,
     },
@@ -47,10 +49,16 @@ export const generateCarImageUrl = (
   url.searchParams.append('angle', `${angle}`);
 
   return `${url}`;
-}; 
+};
 
+export const updateSearchParams = (type: string, value: string) => {
+  const searchParams = new URLSearchParams(window.location.search);
 
+  searchParams.set(type, value);
 
+  const newPathname = `${
+    window.location.pathname
+  }?${searchParams.toString()}`;
 
-
-
+	return newPathname
+};
